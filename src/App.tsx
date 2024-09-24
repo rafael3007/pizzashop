e168c9a93140bfff3c467c77783dcce88c73cc1a
rafael3007 <rafael.brito.1422@gmail.com>
2024-09-23 21:30:51 -0300

    header

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,17 @@ import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 import { Toaster } from 'sonner'
 
+import { ThemeProvider } from './components/Theme/theme-provider'
 import { router } from './Routes'
 
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | Pizza.shop" />
-      <Toaster richColors />
-      <RouterProvider router={router} />
+      <ThemeProvider defaultTheme="dark" storageKey="pizzashop-theme">
+        <Helmet titleTemplate="%s | Pizza.shop" />
+        <Toaster richColors />
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </HelmetProvider>
   )
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,8 @@
 import { Home, Pizza, UtensilsCrossed } from 'lucide-react'
 
+import AccountMenu from '../AccountMenu'
 import NavLink from '../NavLink'
+import { ThemeToggle } from '../Theme/ThemeToggle'
 import { Separator } from '../ui/separator'
 
 export function Header() {
@@ -20,6 +22,10 @@ export function Header() {
             Pedidos
           </NavLink>
         </nav>
+        <div className="ml-auto flex items-center gap-2">
+          <ThemeToggle />
+          <AccountMenu />
+        </div>
       </div>
     </div>
   )
