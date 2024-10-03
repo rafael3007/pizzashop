d70607035835b008abbc3cf849f88eecba17aa9f
rafael3007 <rafael.brito.1422@gmail.com>
2024-10-02 21:06:09 -0300

    order details

---
+++ src/api/get-order-details.ts
@@ -0,0 +1,30 @@
+import { api } from './config/axios'
+
+export interface GetOrderDetailsBody {
+  orderId: string
+}
+
+export interface GetOrdersDetailsResponse {
+  status: 'pending' | 'canceled' | 'processing' | 'delivering' | 'delivered'
+  id: string
+  createdAt: string
+  totalInCents: number
+  customer: {
+    name: string
+    email: string
+    phone: string | null
+  }
+  orderItems: {
+    id: string
+    priceInCents: number
+    quantity: number
+    product: {
+      name: string
+    }
+  }[]
+}
+
+export async function getOrderDetails({ orderId }: GetOrderDetailsBody) {
+  const response = await api.get<GetOrdersDetailsResponse>(`/orders/${orderId}`)
+  return response.data
+}
--- src/pages/app/orders/components/order-table-row.tsx
+++ src/pages/app/orders/components/order-table-row.tsx
@@ -1,6 +1,7 @@
 import { formatDistanceToNow } from 'date-fns'
 import { ptBR } from 'date-fns/locale'
 import { ArrowRight, Search, X } from 'lucide-react'
+import { useState } from 'react'
 
 import { Button } from '@/components/ui/button'
 import { Dialog, DialogTrigger } from '@/components/ui/dialog'
@@ -20,17 +21,18 @@ interface OrderTableRowProps {
 }
 
 export default function OrderTableRow({ order }: OrderTableRowProps) {
+  const [isDetailsOpen, setIsDetailsOpen] = useState(false)
   return (
     <TableRow>
       <TableCell>
-        <Dialog>
+        <Dialog open={isDetailsOpen} onOpenChange={setIsDetailsOpen}>
           <DialogTrigger asChild>
             <Button variant="outline" size="xs">
               <Search className="w-3 h-3" />
               <span className="sr-only">Detalhes do pedido</span>
             </Button>
           </DialogTrigger>
-          <OrderDetails orderID={order.orderId} />
+          <OrderDetails orderId={order.orderId} open={isDetailsOpen} />
         </Dialog>
       </TableCell>
       <TableCell className="font-mono text-xs font-medium">
@@ -47,7 +49,7 @@ export default function OrderTableRow({ order }: OrderTableRowProps) {
       </TableCell>
       <TableCell className="font-medium">{order.customerName}</TableCell>
       <TableCell className="font-medium">
-        {order.total.toLocaleString('pt-BR', {
+        {(order.total / 100).toLocaleString('pt-BR', {
           style: 'currency',
           currency: 'BRL',
         })}
