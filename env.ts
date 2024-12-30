305d96b0b64ce91459ee62d78028543e96f02cdf
Rafael Brito <rafael.brito.1422@gmail.com>
2024-12-30 13:14:53 -0300

    env

--- env.ts
+++ env.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod'
 
 const EnvSchema = z.object({
-  VITE_API_URL: z.string().url(),
+  VITE_API_URL: z.string(),
   VITE_ENABLE_API_DELAY: z
     .string()
     .transform((value: string) => value === 'true'),
