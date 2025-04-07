eb128dd4a0b033e8092053121573b66ddbf190ca
Rafael Brito <rafael.brito.1422@gmail.com>
2025-04-07 12:28:52 -0300

    test: navlink

---
+++ src/components/NavLink/nav-link.spec.tsx
@@ -0,0 +1,24 @@
+import { render } from '@testing-library/react'
+import { MemoryRouter } from 'react-router-dom'
+
+import NavLink from '.'
+
+describe('NavLink', () => {
+  it('should highlight the nav link when is the current page link', () => {
+    // Add your test logic here
+    const wrapper = render(
+      <>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/about">About</NavLink>
+      </>,
+      {
+        wrapper: ({ children }) => (
+          <MemoryRouter initialEntries={['/about']}>{children}</MemoryRouter>
+        ),
+      },
+    )
+
+    expect(wrapper.getByText('About').dataset.current).toEqual('true')
+    expect(wrapper.getByText('Home').dataset.current).toEqual('false')
+  })
+})
