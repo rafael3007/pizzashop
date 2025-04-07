e12b708d9a2a6091ee567775f43667dabb858943
Rafael Brito <rafael.brito.1422@gmail.com>
2025-04-07 15:10:25 -0300

    test: mock authenticate

--- src/api/mocks/index.ts
+++ src/api/mocks/index.ts
@@ -1,8 +1,9 @@
 import { setupWorker } from 'msw/browser'
 
 import { env } from '../../../env.ts'
+import { signInMock } from './sign-in-mock.ts'
 
-export const worrker = setupWorker()
+export const worrker = setupWorker(signInMock)
 
 export async function enableMSW() {
   if (env.MODE !== 'test') {
