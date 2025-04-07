9c1a7cd5bca976b76c7448270303db13438202c0
Rafael Brito <rafael.brito.1422@gmail.com>
2025-04-07 16:53:43 -0300

    test:  mock restaurants

--- src/api/mocks/index.ts
+++ src/api/mocks/index.ts
@@ -1,9 +1,10 @@
 import { setupWorker } from 'msw/browser'
 
 import { env } from '../../../env.ts'
+import { registerRestaurantMock } from './register-restaurant-mock.ts'
 import { signInMock } from './sign-in-mock.ts'
 
-export const worrker = setupWorker(signInMock)
+export const worrker = setupWorker(signInMock, registerRestaurantMock)
 
 export async function enableMSW() {
   if (env.MODE !== 'test') {
