382c36ebee766247162019c2c250fd017a7967fc
Rafael Brito <rafael.brito.1422@gmail.com>
2025-04-08 16:22:05 -0300

    fix: test orders E2E

--- test/orders.e2e-spec.ts
+++ test/orders.e2e-spec.ts
@@ -98,7 +98,7 @@ test('Filter by status', async ({ page }) => {
 
   await page.getByRole('button', { name: 'Filtrar resultados' }).click()
 
-  const tableRows = await page.getByRole('cell', { name: 'Pendente' }).all()
+  const tableRows = await page.getByRole('cell', { name: 'Pendente' })
 
-  expect(tableRows).toHaveLength(10)
+  await expect(tableRows).toHaveCount(10)
 })
